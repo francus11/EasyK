5c8ede956a1685bb95dde7af817741f03dc19da6
francus11 <francus62@gmail.com>
2025-01-14 00:24:25 +0100

    changed name from Macro to MacroAction

---
+++ software/configApp/Actions/MacroAction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace configApp.Actions
+{
+    public class MacroAction : IAction
+    {
+        public List<IAction> ActionList { get; set; }
+
+        public string Label { get; private set; }
+
+        public MacroAction(List<IAction> actions)
+        {
+            ActionList = actions;
+            Label = "Macro";
+        }
+    }
+}
