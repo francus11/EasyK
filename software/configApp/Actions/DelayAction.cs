97cef0b039740489b02a2fd85f0510482bd2b109
francus11 <francus62@gmail.com>
2025-01-14 21:54:41 +0100

    Created DelayAction

---
+++ software/configApp/Actions/DelayAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace configApp.Actions
+{
+    internal class DelayAction : IAction
+    {
+        public int Delay { get; private set; }
+        public string Label { get; private set; };
+
+        public DelayAction(int delay)
+        {
+            Delay = delay;
+            Label = "Delay " + delay + "ms";
+        }
+    }
+}
