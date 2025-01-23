eb9ecd3d9b24cf329f41a3513b8581f4ddbbc662
francus11 <francus62@gmail.com>
2025-01-23 04:34:10 +0100

    Working app(kinda)

--- software/configApp/ConnectDeviceSerialWindow.xaml.cs
+++ software/configApp/ConnectDeviceSerialWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO.Ports;
 using System.Linq;
+using System.Text;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Threading;
@@ -10,16 +12,49 @@ namespace configApp
     public partial class ConnectDeviceSerialWindow : Window
     {
         private DispatcherTimer _portCheckTimer;
+        private DispatcherTimer _connectionCheckTimer;
+        private SerialPort _serialPort;
+        private MainWindow _mainWindow;
+
+        private bool _connectivityCheck = false;
+        public bool ConnectivityCheck { 
+            get
+            {
+                return _connectivityCheck;
+            }
+            set
+            {
+
+                if (value == _connectivityCheck)
+                {
+                    return;
+                }
+
+                _connectivityCheck = value;
+
+                if (_connectivityCheck)
+                {
+                    InitializeConnectionCheckTimer();
+                }
+                else
+                {
+                    _connectionCheckTimer.Stop();
+                }
+
+            }
+        }
+
+        //public RoutedEventHandler DataReceived
 
         public ConnectDeviceSerialWindow()
         {
             InitializeComponent();
             InitializePortCheckTimer();
+            InitializeSerialPort();
         }
 
         private void InitializePortCheckTimer()
         {
-            // Tworzymy timer z interwałem 500 ms
             _portCheckTimer = new DispatcherTimer
             {
                 Interval = TimeSpan.FromMilliseconds(500)
@@ -28,19 +63,43 @@ namespace configApp
             _portCheckTimer.Tick += PortCheckTimer_Tick;
             _portCheckTimer.Start();
         }
+        private void InitializeSerialPort()
+        {
+            _serialPort = new SerialPort
+            {
+                BaudRate = 9600,
+                DataBits = 8,
+                Parity = Parity.None,
+                StopBits = StopBits.One,
+                Encoding = Encoding.ASCII,
+                ReadTimeout = 500,
+                WriteTimeout = 500,
+                RtsEnable = true,
+                DtrEnable = true,
+                //NewLine = "\r\n"
+            };
+
+            //_serialPort.DataReceived += SerialPort_DataReceived;
+        }
+
+        private void InitializeConnectionCheckTimer()
+        {
+            _connectionCheckTimer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(2000)
+            }; ;
+            _connectionCheckTimer.Tick += ConnectionCheckTimer_Elapsed;
+            _connectionCheckTimer.Start();
+        }
 
         private void PortCheckTimer_Tick(object? sender, EventArgs e)
         {
-            // Pobieramy dostępne porty
             string[] availablePorts = SerialPort.GetPortNames();
-
-            // Aktualizujemy ComboBox, jeśli porty się zmieniły
             UpdatePortList(availablePorts);
         }
 
         private void UpdatePortList(string[] availablePorts)
         {
-            // Sprawdzamy, czy lista portów w ComboBoxie jest zgodna z dostępnymi portami
             var currentPorts = PortComboBox.Items.Cast<string>().ToArray();
             if (!availablePorts.SequenceEqual(currentPorts))
             {
@@ -50,7 +109,6 @@ namespace configApp
                     PortComboBox.Items.Add(port);
                 }
 
-                // Jeśli dostępne są porty, wybierz pierwszy na liście
                 if (availablePorts.Length > 0)
                 {
                     PortComboBox.SelectedIndex = 0;
@@ -58,8 +116,65 @@ namespace configApp
             }
         }
 
+        
+
+        private void ConnectionCheckTimer_Elapsed(object? sender, EventArgs e)
+        {
+            if (!_serialPort.IsOpen)
+            {
+                ConnectivityCheck = false;
+                try
+                {
+                    _serialPort.Open();
+                    ConnectivityCheck = true;
+                    return;
+
+                }
+                catch (Exception)
+                {
+                    ConnectivityCheck = false;
+                    Dispatcher.Invoke(() =>
+                    {
+                        MessageBox.Show("Connection lost: Cannot reconnect.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    });
+                }
+
+                this.Show();
+
+                if (sender is ConnectDeviceSerialWindow window)
+                {
+                }
+                _mainWindow.Close();
+                Dispatcher.Invoke(() =>
+                {
+                    MessageBox.Show("Connection lost: Port is closed.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                });
+                return;
+            }
+
+            //try
+            //{
+            //    _serialPort.WriteLine("PING");
+            //}
+            //catch (Exception)
+            //{
+            //    ConnectivityCheck = false;
+            //    _serialPort.Close();
+            //    Dispatcher.Invoke(() =>
+            //    {
+            //        MessageBox.Show("Connection lost: Unable to communicate with the device.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            //    });
+            //}
+        }
+
         private void ConnectButton_Click(object sender, RoutedEventArgs e)
         {
+            if (_serialPort.IsOpen)
+            {
+                MessageBox.Show("Port is already open.", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
             string selectedPort = PortComboBox.SelectedItem?.ToString();
             if (string.IsNullOrEmpty(selectedPort))
             {
@@ -67,14 +182,79 @@ namespace configApp
                 return;
             }
 
-            // Tutaj możesz dodać logikę łączenia z urządzeniem
-            MessageBox.Show($"Connecting to port: {selectedPort}");
+            try
+            {
+                _serialPort.PortName = selectedPort;
+                _serialPort.Open();
+                //MessageBox.Show($"Connected to port: {selectedPort}", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to connect: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            if (_serialPort.IsOpen)
+            {
+                _mainWindow = new MainWindow(_serialPort, this);
+                _mainWindow.Show();
+                this.Hide();
+            }
         }
 
         private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
             _portCheckTimer.Stop();
+            if (_serialPort.IsOpen)
+            {
+                _serialPort.Close();
+            }
             this.Close();
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_serialPort.IsOpen)
+            {
+                MessageBox.Show("Please connect to a port before sending data.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            string dataToSend = SendTextBox.Text;
+            if (string.IsNullOrEmpty(dataToSend))
+            {
+                MessageBox.Show("Please enter data to send.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            try
+            {
+                _serialPort.Write(dataToSend);
+                //MessageBox.Show("Data sent successfully.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to send data: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string receivedData = _serialPort.ReadExisting();
+                Dispatcher.Invoke(() =>
+                {
+                    ReceivedTextBox.AppendText(receivedData);
+                    ReceivedTextBox.ScrollToEnd();
+                });
+            }
+            catch (Exception ex)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    MessageBox.Show($"Failed to receive data: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                });
+            }
         }
     }
 }
--- software/configApp/MainWindow.xaml.cs
+++ software/configApp/MainWindow.xaml.cs
@@ -3,6 +3,9 @@ using configApp.ConfigToolbar;
 using configApp.Controls;
 using configApp.JsonConverters;
 using configApp.UI;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO.Ports;
 using System.Text;
 using System.Text.Json;
 using System.Windows;
@@ -24,15 +27,22 @@ namespace configApp
     {
         JsonSerializerOptions options = new JsonSerializerOptions
         {
-            WriteIndented = true
+            PropertyNameCaseInsensitive = true,
+            //WriteIndented = true
+
         };
 
         private IInputButton? _activeButton = null;
+        private SerialPort _serialPort;
+        private ConnectDeviceSerialWindow _connectDeviceSerialWindow;
 
         public string json = "";
 
-        public MainWindow()
+        public MainWindow(SerialPort serialPort, ConnectDeviceSerialWindow connectDeviceSerialWindow)
         {
+            _serialPort = serialPort;
+            _connectDeviceSerialWindow = connectDeviceSerialWindow;
+
             InitializeComponent();
             options.Converters.Add(new IControlListJsonConverter());
             options.Converters.Add(new MacroActionJsonConverter());
@@ -40,11 +50,79 @@ namespace configApp
             options.Converters.Add(new ButtonControlJsonConverter());
             options.Converters.Add(new DelayActionJsonConverter());
             options.Converters.Add(new IActionJsonConverter());
+            options.Converters.Add(new EncoderControlJsonConverter());
+
+            if (!InitializeConfiguration())
+            {
+                // Jeśli nie udało się pobrać konfiguracji, zamykamy okno
+                MessageBox.Show("Failed to retrieve configuration. Closing the application.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                Close();
+            }
+        }
+
+        private bool InitializeConfiguration()
+        {
+
+            //TODO add verification of type of device
+            _connectDeviceSerialWindow.ConnectivityCheck = false;
+            try
+            {
+                if (!_serialPort.IsOpen)
+                {
+                    MessageBox.Show("Serial port is not open. Unable to retrieve configuration.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return false;
+                }
+
+                _serialPort.ReadTimeout = 5000;
+                _serialPort.Write("getConfiguration");
+
+                string response = _serialPort.ReadLine();
+
+                ProcessConfiguration(response);
+                _connectDeviceSerialWindow.ConnectivityCheck = true;
+
+                return true;
+            }
+            catch (TimeoutException)
+            {
+                MessageBox.Show("Timeout while waiting for configuration.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+        }
+
+        private void ProcessConfiguration(string json)
+        {
+            try
+            {
+                List<IControl> config = JsonSerializer.Deserialize<List<IControl>>(json, options);
+                if (config != null && config.Count == 17)
+                {
+                    for (int i = 0; i < 16; i++)
+                    {
+                        if (ButtonsMatrixGrid.Children[i] is IInputButton button)
+                        {
+                            button.SavedControl = config[i];
+                        }
+                    }
+
+                    EncoderElement.SavedControl = config[16];
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Invalid configuration format: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void OpenNewWindow_Click(object sender, RoutedEventArgs e)
         {
-            
+            ConnectDeviceSerialWindow window = new ConnectDeviceSerialWindow();
+            window.Show();
         }
 
         private void KeyButton_Click(object sender, RoutedEventArgs e)
@@ -93,8 +171,10 @@ namespace configApp
             NotSelectedElementConfig.Visibility = Visibility.Hidden;
             BasicToolbar toolbar = new BasicToolbar()
             {
-                Control = _activeButton.SavedControl
+                Control = _activeButton.SavedControl,
+                
             };
+            toolbar.SaveClicked += (s, e) => SaveButton_Click(s, e);
             ToolbarSectionGrid.Children.Clear();
             ToolbarSectionGrid.Children.Add(toolbar);
         }
@@ -109,6 +189,75 @@ namespace configApp
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            _connectDeviceSerialWindow.ConnectivityCheck = false;
+            Thread.Sleep(1000);
+            _activeButton.SavedControl = ((BasicToolbar)sender).Control;
+
+            List<IControl> controls = ButtonsMatrixGrid.Children.OfType<IInputButton>().Select(b => b.SavedControl).ToList();
+            controls.Add(EncoderElement.SavedControl);
+
+            json = JsonSerializer.Serialize(controls, options);
+            Debug.WriteLine(json);
+
+            string response;
+            try
+            {
+                if (!_serialPort.IsOpen)
+                {
+                    MessageBox.Show("Serial port is not open. Unable to save configuration.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                int oldTimeout = _serialPort.ReadTimeout;
+                _serialPort.Write("setConfiguration");
+                //_serialPort.WriteLine(json);
+
+                response = _serialPort.ReadLine().Replace("\r", "");
+                if (response == "ready")
+                {
+                    _serialPort.Write(json);
+                    response = _serialPort.ReadLine().Replace("\r", "");
+                    if(response == "completed")
+                    {
+                        MessageBox.Show("Configuration saved successfully.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Failed to save configuration.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+
+
+            _connectDeviceSerialWindow.ConnectivityCheck = true;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var result = MessageBox.Show(
+                "Are you sure you want to close application?",
+                "Closing application",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question
+            );
+
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                _connectDeviceSerialWindow.Close();
+            }
         }
     }
 }
